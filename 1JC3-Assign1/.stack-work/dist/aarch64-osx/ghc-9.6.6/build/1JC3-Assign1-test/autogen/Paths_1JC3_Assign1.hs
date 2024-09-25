ee5293d419ad9557c99f60107d68f37c902208a9
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2024-09-25 09:16:20 -0400

    finished objective assign 1

---
+++ 1JC3-Assign1/.stack-work/dist/aarch64-osx/ghc-9.6.6/build/1JC3-Assign1-test/autogen/Paths_1JC3_Assign1.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NoRebindableSyntax #-}
+#if __GLASGOW_HASKELL__ >= 810
+{-# OPTIONS_GHC -Wno-prepositive-qualified-module #-}
+#endif
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+{-# OPTIONS_GHC -w #-}
+module Paths_1JC3_Assign1 (
+    version,
+    getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+
+import qualified Control.Exception as Exception
+import qualified Data.List as List
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0,1,0,0] []
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir `joinFileName` name)
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+
+
+
+
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
+bindir     = "/Users/ameyagupta/Desktop/repos/compsci-1jc3/1JC3-Assign1/.stack-work/install/aarch64-osx/b48f9f8f47818380b092a2a1809561be7a06924a4eef67cc3e0d1b3d581436df/9.6.6/bin"
+libdir     = "/Users/ameyagupta/Desktop/repos/compsci-1jc3/1JC3-Assign1/.stack-work/install/aarch64-osx/b48f9f8f47818380b092a2a1809561be7a06924a4eef67cc3e0d1b3d581436df/9.6.6/lib/aarch64-osx-ghc-9.6.6/1JC3-Assign1-0.1.0.0-81wheZqKZjpIazXFNzYtvs-1JC3-Assign1-test"
+dynlibdir  = "/Users/ameyagupta/Desktop/repos/compsci-1jc3/1JC3-Assign1/.stack-work/install/aarch64-osx/b48f9f8f47818380b092a2a1809561be7a06924a4eef67cc3e0d1b3d581436df/9.6.6/lib/aarch64-osx-ghc-9.6.6"
+datadir    = "/Users/ameyagupta/Desktop/repos/compsci-1jc3/1JC3-Assign1/.stack-work/install/aarch64-osx/b48f9f8f47818380b092a2a1809561be7a06924a4eef67cc3e0d1b3d581436df/9.6.6/share/aarch64-osx-ghc-9.6.6/1JC3-Assign1-0.1.0.0"
+libexecdir = "/Users/ameyagupta/Desktop/repos/compsci-1jc3/1JC3-Assign1/.stack-work/install/aarch64-osx/b48f9f8f47818380b092a2a1809561be7a06924a4eef67cc3e0d1b3d581436df/9.6.6/libexec/aarch64-osx-ghc-9.6.6/1JC3-Assign1-0.1.0.0"
+sysconfdir = "/Users/ameyagupta/Desktop/repos/compsci-1jc3/1JC3-Assign1/.stack-work/install/aarch64-osx/b48f9f8f47818380b092a2a1809561be7a06924a4eef67cc3e0d1b3d581436df/9.6.6/etc"
+
+getBinDir     = catchIO (getEnv "1JC3_Assign1_bindir")     (\_ -> return bindir)
+getLibDir     = catchIO (getEnv "1JC3_Assign1_libdir")     (\_ -> return libdir)
+getDynLibDir  = catchIO (getEnv "1JC3_Assign1_dynlibdir")  (\_ -> return dynlibdir)
+getDataDir    = catchIO (getEnv "1JC3_Assign1_datadir")    (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "1JC3_Assign1_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "1JC3_Assign1_sysconfdir") (\_ -> return sysconfdir)
+
+
+
+joinFileName :: String -> String -> FilePath
+joinFileName ""  fname = fname
+joinFileName "." fname = fname
+joinFileName dir ""    = dir
+joinFileName dir fname
+  | isPathSeparator (List.last dir) = dir ++ fname
+  | otherwise                       = dir ++ pathSeparator : fname
+
+pathSeparator :: Char
+pathSeparator = '/'
+
+isPathSeparator :: Char -> Bool
+isPathSeparator c = c == '/'
--- 1JC3-Assign1/src/Assign_1.hs
+++ 1JC3-Assign1/src/Assign_1.hs
@@ -1,16 +1,15 @@
-{-|
-Module      : 1JC3-Assign1.Assign_1.hs
-Copyright   :  (c) Curtis D'Alves 2022
-License     :  GPL (see the LICENSE file)
-Maintainer  :  none
-Stability   :  experimental
-Portability :  portable
-
-Description:
-  Assignment 1 - McMaster CS 1JC3 2024.
-
-  Modified by W. M. Farmer 19-SEP-2024.
--}
+-- |
+-- Module      : 1JC3-Assign1.Assign_1.hs
+-- Copyright   :  (c) Curtis D'Alves 2022
+-- License     :  GPL (see the LICENSE file)
+-- Maintainer  :  none
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Description:
+--   Assignment 1 - McMaster CS 1JC3 2024.
+--
+--   Modified by W. M. Farmer 19-SEP-2024.
 module Assign_1 where
 
 -----------------------------------------------------------------------------------------------------------
@@ -31,32 +30,32 @@ macid = "gupta67"
 
 -- for reference, a power operator for negative numbers, since powers on these numbers can often result in nonsensical results
 (***) :: Double -> Double -> Double
-x *** y = if x >= 0 
-            then x ** y 
-            else -( (-x) ** y)
+x *** y =
+  if x >= 0
+    then x ** y
+    else -((-x) ** y)
 
 -- calculates the cube root of some Double, x
 cbrt :: Double -> Double
-cbrt x = x *** (1/3)
-
+cbrt x = x *** (1 / 3)
 
 {- -----------------------------------------------------------------
  - cubicQ
  - -----------------------------------------------------------------
  - Description:
- -   cubicQ takes in 3 Double parameters - a, b, and c (coefficients in a cubic function) - and returns a Double value that is used to calculate the discriminant of the equation 
+ -   cubicQ takes in 3 Double parameters - a, b, and c (coefficients in a cubic function) - and returns a Double value that is used to calculate the discriminant of the equation
  -}
 cubicQ :: Double -> Double -> Double -> Double
-cubicQ a b c = ((3 * a * c) - (b *** 2)) / (9 * (a *** 3))
+cubicQ a b c = ((3 * a * c) - (b ** 2)) / (9 * (a ** 2))
 
 {- -----------------------------------------------------------------
  - cubicR
  - -----------------------------------------------------------------
  - Description:
- -   cubicR takes in 4 Double parameters - a, b, c, and d - and returns a Double value that is used in conjunction with the value from cubicQ to calculate the discriminant
+ -   cubicR takes in 4 Double parameters - a, b, c, and d (coefficients in a cubic function) - and returns a Double value that is used in conjunction with the value from cubicQ to calculate the discriminant
  -}
 cubicR :: Double -> Double -> Double -> Double -> Double
-cubicR a b c d = ((9 * a * b * c) - (27 * (a *** 2) * d) - (2 * (b *** 3))) / (54 * (a *** 3))
+cubicR a b c d = ((9 * a * b * c) - (27 * (a ** 2) * d) - (2 * (b ** 3))) / (54 * (a ** 3))
 
 {- -----------------------------------------------------------------
  - cubicDiscSign
@@ -65,12 +64,12 @@ cubicR a b c d = ((9 * a * b * c) - (27 * (a *** 2) * d) - (2 * (b *** 3))) / (5
  -   calculates the discriminant via the results of cubicQ and cubicR, and returns its sign as an integer in { -1, 0, 1 }
  -}
 cubicDiscSign :: Double -> Double -> Int
-cubicDiscSign q r 
-  | discrim < 0       = -1 
+cubicDiscSign q r
+  | discrim < 0 = -1
   | abs discrim === 0 = 0
-  | otherwise         = 1
+  | otherwise = 1
   where
-    discrim = q***3 + r***2
+    discrim = (q ** 3) + (r ** 2)
 
 {- -----------------------------------------------------------------
  - cubicS
@@ -79,9 +78,9 @@ cubicDiscSign q r
  -   calculates the value S in Cardano's Formula to help determine the roots of the cubic function by using the helper values Q and R from cubicQ and cubicR respectively
  -}
 cubicS :: Double -> Double -> Double
-cubicS q r = let discrim = q***3 + r***2
-             in cbrt ( r + discrim***(1/2) )
-                
+cubicS q r = cbrt (r + sqrt discrim)
+  where
+    discrim = (q ** 3) + (r ** 2)
 
 {- -----------------------------------------------------------------
  - cubicT
@@ -90,9 +89,9 @@ cubicS q r = let discrim = q***3 + r***2
  -   calculates the value T in Cardano's Formula to help determine the roots of the cubic function by using the helper values Q and R from cubicQ and cubicR respectively
  -}
 cubicT :: Double -> Double -> Double
-cubicT q r = cbrt ( r - discrim***(1/2) )
-             where
-                discrim = q***3 + r***2
+cubicT q r = cbrt (r - sqrt discrim)
+  where
+    discrim = (q ** 3) + (r ** 2)
 
 {- -----------------------------------------------------------------
  - cubicRealSolutions
@@ -102,26 +101,24 @@ cubicT q r = cbrt ( r - discrim***(1/2) )
  -}
 cubicRealSolutions :: Double -> Double -> Double -> Double -> [Double]
 cubicRealSolutions a b c d
-  | a == 0      = []
-  | sign == -1  = []
-  | sign ==  0  = [
-                    ( s + t - (b / (3 * a)) ),
-                    ( -((s + t)/2) - (b / (3 * a))  )
-                    ]
-  | sign ==  1  = error "TODO"
-  | otherwise   = []
+  | a == 0 = []
+  | sign == -1 = []
+  | sign == 0 = if x1 === x2 then [x1, x1, x1] else [x1, x2, x2]
+  | sign == 1 = [x1]
+  | otherwise = []
   where
     sign = cubicDiscSign q r
-    s    = cubicS q r
-    t    = cubicT q r
-    q    = cubicQ a b c
-    r    = cubicR a b c d
-
+    s = cubicS q r
+    t = cubicT q r
+    q = cubicQ a b c
+    r = cubicR a b c d
+    x1 = s + t - (b / (3 * a))
+    x2 = -((s + t) / 2) - (b / (3 * a)) -- + (sqrt (-3) * (s - t) / 2) -- not necessary
 
 (===) :: Double -> Double -> Bool
-x === y = let
-  tol = 1e-3
-  in abs (x-y) <= tol
+x === y =
+  let tol = 1e-3
+   in abs (x - y) <= tol
 
 {- -----------------------------------------------------------------
  - Test Cases
